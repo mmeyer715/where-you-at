45e2bf5acc96de4702ef6ba4d7f8f718a19d3faa
Dolo_Mane <zimmerman.lucas@hotmail.com>
2022-04-26 17:20:24 -0500

    Create seed file. resolve schema.sql issues

--- db/schema.sql
+++ db/schema.sql
@@ -22,6 +22,10 @@ CREATE TABLE employee (
   id INT AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) ,
   last_name VARCHAR(30) ,
-  role_id INTEGER FOREIGN KEY REFERENCES job_role(id),
-  manager_id INTEGER FOREIGN KEY REFERENCES employee(id)
+  role_id INTEGER,
+  manager_id INTEGER, 
+  FOREIGN KEY(role_id) REFERENCES job_role(id)
+    ON DELETE SET NULL, 
+  FOREIGN KEY(manager_id) REFERENCES employee(id)
+    ON DELETE SET NULL
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -14,8 +14,16 @@ VALUES
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
-    ('Lucas', 'Zimmerman', 1, 2),
-    ('Maria', 'Meyer', 2, 1),
-    ('Zachary', 'Meyer', 4, 3),
-    ('Cora', 'Bean', 3, 4),
+    ('Lucas', 'Zimmerman', 1, NULL),
+    ('Maria', 'Meyer', 2, NULL),
+    ('Zachary', 'Meyer', 4, NULL),
+    ('Cora', 'Bean', 3, NULL),
     ('Zayne', 'Nguyen', 2, NULL);
+
+UPDATE employee SET manager_id = 2 WHERE id = 1;
+
+UPDATE employee SET manager_id = 1 WHERE id = 2;
+
+UPDATE employee SET manager_id = 2 WHERE id = 3;
+
+UPDATE employee SET manager_id = 5 WHERE id = 4;
