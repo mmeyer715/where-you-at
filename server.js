dd8732e544b53399767dfd708dce2060bf5384ac
Dolo_Mane <zimmerman.lucas@hotmail.com>
2022-04-27 19:31:03 -0500

    remove routes

--- server.js
+++ server.js
@@ -1,67 +1,6 @@
 // required packages
-const express = require('express');
 const mysql = require('mysql2');
 const dbEnv = require('dotenv').config();
-
-const PORT = process.env.PORT || 3001;
-const app = express();
-
-// express middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-
-// Get All Departments
-app.get('/api/departments', (req, res) => {
-    const sql = `SELECT * FROM department`;
-  
-    db.query(sql, (err, rows) => {
-      if (err) {
-        res.status(500).json({ error: err.message });
-        return;
-      }
-      res.json({
-        message: 'success',
-        data: rows
-      });
-    });
-});
-
-// Get All Roles
-app.get('/api/roles', (req, res) => {
-    const sql = `SELECT * FROM job_role`;
-  
-    db.query(sql, (err, rows) => {
-      if (err) {
-        res.status(500).json({ error: err.message });
-        return;
-      }
-      res.json({
-        message: 'success',
-        data: rows
-      });
-    });
-});
-
-// Get All Employees
-app.get('/api/employees', (req, res) => {
-    const sql = `SELECT * FROM employee`;
-  
-    db.query(sql, (err, rows) => {
-      if (err) {
-        res.status(500).json({ error: err.message });
-        return;
-      }
-      res.json({
-        message: 'success',
-        data: rows
-      });
-    });
-});
-
-// Default response for any other request (Not Found)
-app.use((req, res) => {
-    res.status(404).end();
-});
 
 // connect to db
 const db = mysql.createConnection(
@@ -74,7 +13,17 @@ const db = mysql.createConnection(
     console.log('Connected to the ' + process.env.DB_NAME  + ' database!')
 );
 
-app.listen(PORT, () => {
-    console.log(`Server running on ${PORT}!`);
-})
+// Get all departments
+// db.query(`SELECT * FROM department`, (err, rows) => {
+//   console.log(rows);
+// });
 
+// Get all roles
+// db.query(`SELECT * FROM job_role`, (err, rows) => {
+//   console.log(rows);
+// });
+
+// Get all employees
+// db.query(`SELECT * FROM employee`, (err, rows) => {
+//   console.log(rows);
+// });
